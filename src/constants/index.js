94d2a4b370d47e0782423502e3c2ea3d644f05a6
unknown <cherish0948@gmail.com>
2023-09-12 22:57:10 +0900

    사이트 완료

---
+++ src/constants/index.js
@@ -0,0 +1,69 @@
+export const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
+    },
+    {
+        title: "skill",
+        url: "#skill"
+    },
+    {
+        title: "site",
+        url: "#site"
+    },
+    {
+        title: "portfolio",
+        url: "#port"
+    },
+    {
+        title: "contact",
+        url: "#contact"
+    }
+];
+
+export const siteText = [
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 활용한 사이트 제작",
+        code: "https://github.com/Youngveloper/react-site",
+        view: "https://github.com/Youngveloper/react-site",
+        info: [
+            "site coding",
+            "production period: two days",
+            "use stack: HTML5/CSS3, CSS Variable, React"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 활용한 사이트 제작",
+        code: "https://github.com/Youngveloper/react-site",
+        view: "https://github.com/Youngveloper/react-site",
+        info: [
+            "site coding",
+            "production period: two days",
+            "use stack: HTML5/CSS3, CSS Variable, React"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 활용한 사이트 제작",
+        code: "https://github.com/Youngveloper/react-site",
+        view: "https://github.com/Youngveloper/react-site",
+        info: [
+            "site coding",
+            "production period: two days",
+            "use stack: HTML5/CSS3, CSS Variable, React"
+        ]
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 활용한 사이트 제작",
+        code: "https://github.com/Youngveloper/react-site",
+        view: "https://github.com/Youngveloper/react-site",
+        info: [
+            "site coding",
+            "production period: two days",
+            "use stack: HTML5/CSS3, CSS Variable, React"
+        ]
+    }
+];
